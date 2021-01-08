815fe36f8b1d2d23cd2dc27a75052a692578c545
jackhowa <jack.howard@washpost.com>
2021-01-08 01:04:39 +0000

    Update bugfixes per suggestion
    https://github.com/babel/preset-modules#babelpreset-modules

--- app.modern.mjs
+++ app.modern.mjs
@@ -1,27 +1,17 @@
 "use strict";
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+const destructureThisArr = ['testing', 'esmodules']; // destructure
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+const newThingAddedArr = [...destructureThisArr, 'tonight']; // not sure about backticks
 
-var destructureThisArr = ['testing', 'esmodules']; // destructure
-
-var newThingAddedArr = [...destructureThisArr, 'tonight']; // not sure about backticks
-
-var targetOutput = "".concat(newThingAddedArr.join(' ')); // arrow func es2017
+const targetOutput = `${newThingAddedArr.join(' ')}`; // arrow func es2017
 // const check
 // async
 
-var arbitraryFlex = /*#__PURE__*/function () {
-  var _ref = _asyncToGenerator(function* (county) {
-    console.log(targetOutput);
-    console.log(county);
-  });
-
-  return function arbitraryFlex(_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
+const arbitraryFlex = async county => {
+  console.log(targetOutput);
+  console.log(county);
+};
 
 class Location {
   constructor(county) {
@@ -30,5 +20,5 @@ class Location {
 
 }
 
-var currentLocation = new Location('West Sussex');
+const currentLocation = new Location('West Sussex');
 arbitraryFlex(currentLocation.county);
