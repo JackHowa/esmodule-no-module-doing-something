8ca0ad4a88ea56826e8f8194e060c63c7e84314c
jackhowa <jack.howard@washpost.com>
2021-01-09 16:33:19 +0000

    Remove spec formatting for arrow func

--- legacy-webpack.config.js
+++ legacy-webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
                 },
               ],
             ],
-            plugins: ['@babel/plugin-transform-runtime', ['@babel/plugin-transform-arrow-functions', { spec: true }]],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-arrow-functions'],
           }
         }
       }
