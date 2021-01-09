032cf247f43b2c91399b2d2c85a58231bf62e3de
jackhowa <jack.howard@washpost.com>
2021-01-09 16:29:54 +0000

    Add environment info for es5 https://webpack.js.org/configuration/output/#outputenvironment

--- legacy-webpack.config.js
+++ legacy-webpack.config.js
@@ -7,7 +7,20 @@ module.exports = {
     filename: 'app.legacy.js',
     path: path.resolve(__dirname, 'dist'),
     environment: {
-      arrowFunction: false
+      // The environment supports arrow functions ('() => { ... }').
+      arrowFunction: false,
+      // The environment supports BigInt as literal (123n).
+      bigIntLiteral: false,
+      // The environment supports const and let for variable declarations.
+      const: false,
+      // The environment supports destructuring ('{ a, b } = obj').
+      destructuring: false,
+      // The environment supports an async import() function to import EcmaScript modules.
+      dynamicImport: false,
+      // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+      forOf: false,
+      // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+      module: false,
     }
   },
   module: {
